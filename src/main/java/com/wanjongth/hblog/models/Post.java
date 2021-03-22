5313971ba66828063d1d678e1b909cc750b3950b
jongwanhong <infinity4869@gmail.com>
2021-03-22 15:45:55 +0900

    application - enable Jpa, spring boot / Post - declarate var / Repo - extends JpaRepo

--- src/main/java/com/wanjongth/hblog/models/Post.java
+++ src/main/java/com/wanjongth/hblog/models/Post.java
@@ -1,4 +1,23 @@
 package com.wanjongth.hblog.models;
 
-public class Post {
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+public class Post extends Timestamped{
+
+    // ID 자동 생성 및 증가
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Id
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String honor;
+
+    @Column(nullable = false)
+    private String contents;
 }
--- src/main/java/com/wanjongth/hblog/models/PostRepository.java
+++ src/main/java/com/wanjongth/hblog/models/PostRepository.java
@@ -1,4 +1,6 @@
 package com.wanjongth.hblog.models;
 
-public interface PostRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostRepository extends JpaRepository<Post, Long>{
 }
