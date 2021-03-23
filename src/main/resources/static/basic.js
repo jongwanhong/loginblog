999e0c8f6024d24cbadd782939ce109b89ff369c
jongwanhong <infinity4869@gmail.com>
2021-03-23 22:47:06 +0900

    fixed get specific-post-modal

--- src/main/resources/static/basic.js
+++ src/main/resources/static/basic.js
@@ -5,7 +5,7 @@ function isValidContents(contents) {
         return false;
     }
     if (contents.trim().length > 140) { // trim - 앞,뒤 공백 제거
-        alert('공백 포함 140자 이하로 입력해주세요');
+        alert('내용을 공백 포함 140자 이하로 입력해주세요');
         return false;
     }
     return true;
@@ -18,7 +18,7 @@ function isValidTitle(title) {
         return false;
     }
     if (title.trim().length > 30) {
-        alert('공백 포함 30자 이하로 입력해주세요');
+        alert('제목을 공백 포함 30자 이하로 입력해주세요');
         return false;
     }
     return true;
@@ -31,7 +31,7 @@ function isValidAuthor(author) {
         return false;
     }
     if (author.trim().length > 10) {
-        alert('공백 포함 10자 이하로 입력해주세요');
+        alert('이름을 공백 포함 10자 이하로 입력해주세요');
         return false;
     }
     return true;
@@ -77,7 +77,27 @@ function getMessages() {
         type: 'GET',
         url: '/api/posts',
         success: function (response) {
-            for (let i=0; i<response.length; i++){
+            for (let i = 0; i < response.length; i++) {
+                let post = response[i];
+                let id = post.id;
+                let title = post.title;
+                let author = post.author;
+                let contents = post.contents;
+                let modifiedAt = post.modifiedAt;
+                addHTML(id, title, author, contents, modifiedAt)
+            }
+        }
+
+    })
+}
+
+function getMessages2() {
+    // 2. 포스트 목록을 불러와서 HTML로 붙입니다.
+    $.ajax({
+        type: 'GET',
+        url: '/api/posts',
+        success: function (response) {
+            for (let i = 0; i < response.length; i++) {
                 let post = response[i];
                 let id = post.id;
                 let title = post.title;
@@ -96,73 +116,98 @@ function getMessages() {
 function addHTML(id, title, author, contents, modifiedAt) {
     // 1. HTML 태그를 만듭니다.
     let tempHtml = `<div class="card">
-            <!-- date/username 영역 -->
-            <div class="metadata">
-                <div id="${id}-author" class="author">
-                    ${author}
-                </div>
-                <div class="date">
-                    ${modifiedAt}
-                </div>
-            </div>
-            <div id="${id}-title" class="title" data-toggle="modal" data-target="#modal-post">
-                ${title}
-            </div>
-            <!-- 수정 영역 -->
-            <div class="contents">
-
-                <div id="${id}-editarea" class="edit">
-                    <textarea id="${id}-textarea" class="te-edit" name="" id="" cols="30" rows="5"></textarea>
-                </div>
-            </div>
-            <div class="footer">
-                <img id="${id}-edit" onclick="editPost('${id}')" class="icon-start-edit" src="images/edit.png"
-                     alt="">
-                <img id="${id}-delete" onclick="deleteOne('${id}')" class="icon-delete" src="images/delete.png"
-                     alt="">
-                <img id="${id}-submit" onclick="submitEdit('${id}')" class="icon-end-edit" src="images/done.png"
-                     alt="">
-            </div>
-        </div>
-        <!-- 모달 포스트 -->
-        <div class="modal fade" id="modal-post" role="dialog" aria-labelledby="introHeader" aria-hidden="true"
-             tabindex="-1">
-            <div class="modal-dialog">
-                <div class="modal-content">
-                    <div class="modal-header">
-                            <div id="${id}-title" class="title">
-                                ${title}
+                        <!-- date/username 영역 -->
+                        <div class="metadata">
+                            <div id="${id}-author" class="author">
+                                ${author}
                             </div>
-                            <div class="metadata">
-                                <div id="${id}-author" class="author">
-                                    ${author}
-                                </div>
-                                <div class="date">
-                                    ${modifiedAt}
-                                </div>
+                            <div class="date">
+                                ${modifiedAt}
                             </div>
+                        </div>
+                        <div id="${id}-title" class="title" data-toggle="modal" data-target="#${id}">
+                            ${title}
+                        </div>
+                        <!-- 수정 영역 -->
+                        <div class="contents">
+            
+                            <div id="${id}-editarea" class="edit">
+                                <textarea id="${id}-textarea" class="te-edit" cols="30" rows="5"></textarea>
+                            </div>
+                        </div>
+                        <div class="footer">
+                            <img id="${id}-edit" onclick="editPost('${id}')" class="icon-start-edit" src="images/edit.png"
+                                 alt="">
+                            <img id="${id}-delete" onclick="deleteOne('${id}')" class="icon-delete" src="images/delete.png"
+                                 alt="">
+                            <img id="${id}-submit" onclick="submitEdit('${id}')" class="icon-end-edit" src="images/done.png"
+                                 alt="">
+                        </div>
                     </div>
-                    <div class="modal-body">
-                        <!-- 아래 영역-->
-                        <div class="footer card-body text-secondary">
-                            <div id="${id}-contents" class="contents">
-                                ${contents}
+                    <!-- 모달 포스트 -->
+                     <div class="modal fade" id="${id}" role="dialog" aria-labelledby="introHeader" aria-hidden="true"
+                         tabindex="-1">
+                        <div class="modal-dialog">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                        <div id="${id}-title" class="title">
+                                            ${title}
+                                        </div>
+                                        <div class="metadata">
+                                            <div id="${id}-author" class="author">
+                                                ${author}
+                                            </div>
+                                            <div class="date">
+                                                ${modifiedAt}
+                                            </div>
+                                        </div>
+                                </div>
+                                <div class="modal-body">
+                                    <!-- 아래 영역-->
+                                    <div class="footer card-body text-secondary">
+                                        <div id="${id}-contents" class="contents">
+                                            ${contents}
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-default" data-dismiss="modal">닫기</button>
+                                </div>
                             </div>
                         </div>
                     </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-default" data-dismiss="modal">닫기</button>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>`;
+                </div>`;
     // 2. #cards-box 에 HTML을 붙인다.
     $('#cards-box').append(tempHtml);
 }
 
 
-// 메모를 생성합니다.
+// 포스트를 수정합니다.
+function submitEdit(id) {
+    // 1. 작성 대상 포스트의 author와 contents 를 확인합니다.
+    let title = $(`#${id}-title`).text().trim();
+    let author = $(`#${id}-author`).text().trim();
+    let contents = $(`#${id}-textarea`).val().trim();
+    // 2. 작성한 포스트가 올바른지 isValidContents 함수를 통해 확인합니다.
+    if (isValidContents(contents) == false) {
+        return;
+    }
+    // 3. 전달할 data JSON으로 만듭니다.
+    let data = {'title': title, 'author': author, 'contents': contents};
+    // 4. PUT /api/memos/{id} 에 data를 전달합니다.
+    $.ajax({
+        type: "PUT",
+        url: `/api/posts/${id}`,
+        contentType: "application/json",
+        data: JSON.stringify(data),
+        success: function (response) {
+            alert('메시지 변경에 성공하였습니다.');
+            window.location.reload();
+        }
+    });
+}
+
+// 포스트 생성합니다.
 function writePost() {
     // 1. 작성한 메모를 불러옵니다.
     let contents = $('#contents').val();
