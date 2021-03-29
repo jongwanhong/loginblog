ab57ead0ad1b5effd9813c2c4420a95330857a82
jongwanhong <infinity4869@gmail.com>
2021-03-29 13:17:33 +0900

    dev env setting

---
+++ src/main/java/wanjongth/loginblog/LoginblogApplication.java
@@ -0,0 +1,13 @@
+package wanjongth.loginblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginblogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoginblogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/wanjongth/loginblog/LoginblogApplicationTests.java
@@ -0,0 +1,13 @@
+package wanjongth.loginblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginblogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
