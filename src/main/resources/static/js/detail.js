ad6104fd7afdb782940a5add31391e21204bd2aa
jongwanhong <infinity4869@gmail.com>
2021-04-01 02:27:06 +0900

    modify PUT & DELETE / css

--- src/main/resources/static/js/detail.js
+++ src/main/resources/static/js/detail.js
@@ -1,8 +1,6 @@
 $(document).ready(function () {
     backHome()
-    getId()
-    showHide()
-    deleteOne()
+    getDetail()
     $('.comment__card-box').empty();
 })
 
@@ -32,25 +30,18 @@ function isValidTitle(title) {
     return true;
 }
 
-
-function getId() {
-    let id = location.search.split('=')[1]
-    getDetail(id)
-    getComment(id)
-}
-
 function backHome() {
     $('#home').on('click', function () {
         window.location.href = "/"
     })
 }
 
-function getDetail(id) {
-    // $('.content_container').empty();
-    let idx = id
+function getDetail() {
+    let id = location.search.split('=')[1]
+    $('#cards-box').empty();
     $.ajax({
         type: 'GET',
-        url: `/api/detail/${idx}`,
+        url: `/api/detail/${id}`,
         success: function (response) {
             addDetail(response['id'], response['author'], response['title'], response['contents'], response['modifiedAt'])
         }
@@ -59,64 +50,73 @@ function getDetail(id) {
 
 function addDetail(id, author, title, contents, modifiedAt) {
     let tempHtml = `<div id="cards-box">
-        <div class ="card">
-            <div class="card-body">
-                <h2 class="title">${title}</h2>
-                <p class="post-author metadata">
-                    <span class="author">${author}</span> <span class="date">${modifiedAt}</span>
-                </p>
-            </div>
-            <div class="card-text">
-                <p class="contents">
-                    ${contents}
-                </p>
-            </div>
-        </div>
-    </div>`
+                        <div class ="card">
+                            <div class="card-body">
+                                <h2 id="title" class="title">${title}</h2>
+                                <p class="post-author metadata">
+                                    <span id = "author" class="author">${author}</span> <span class="date">${modifiedAt}</span>
+                                </p>
+                            </div>
+                            <div class="card-text">
+                                <p id = "contents" class="contents">
+                                    ${contents}
+                                </p>
+                            </div>
+                            <!-- 수정 영역 -->
+                            <div class="contents">
+                            <div id="editarea" class="edit">
+                                <textarea id="textarea" class="te-edit" cols="30" rows="5"></textarea>
+                            </div>
+                        </div>
+                        <div class="footer">
+                            <img id="edit" onclick="editPost()" class="icon-start-edit" src="images/edit.png"
+                                 alt="">
+                            <img id="delete" onclick="deleteOne()" class="icon-delete" src="images/delete.png"
+                                 alt="">
+                            <img id="submit" onclick="submitEdit()" class="icon-end-edit" src="images/done.png"
+                                 alt="">
+                        </div>
+                        </div>
+                
+                    </div>`
     $('#cards-box').append(tempHtml)
 }
 
-function showHide() {
-    $('#edit').on('click', function () {
-        let username = $('.username').text()
-        let cur_username = $('.username01').text()
-        if (cur_username != username) {
-            alert("자신이 작성한 글만 수정이 가능합니다!")
-            return;
-        }
-        $('.detail').hide()
-        $('.detail__edit').show()
-        let title = $('.title').text()
-        let contents = $('.contents').text().trim()
-        let author = $('.post-author').text().trim()
-        $('.post-author-edit').text(author)
-        $('.detail-input').val(title)
-        $('.detail-textarea').val(contents)
-    })
-    $('.cancel').on('click', function () {
-        $('.detail').show()
-        $('.detail__edit').hide()
-        $('.detail-input').val('')
-        $('.detail-textarea').val('')
-    })
+function editPost() {
+    showEdits();
+    let contents = $(`.contents`).text().trim();
+    $(`#textarea`).val(contents);
 }
 
-// 포스트를 수정합니다.
+function showEdits() {
+    $(`#editarea`).show();
+    $(`#submit`).show();
+    $(`#delete`).show();
+
+    $(`#contents`).hide();
+    $(`#edit`).hide();
+}
+
+// // 포스트를 수정합니다.
 function submitEdit() {
+    // 게시물 번호를 가져옵니다
     let id = location.search.split('=')[1]
-    let title = $('.detail-input').text().trim();
-    let contents = $('.detail-textarea').val().trim();
-    let author = $('.author').text().trim();
+    // 제목, 내용, 작성자를 가져옵니다.
+    let title = $(`#title`).text();
+    let contents = $(`#textarea`).val().trim();
+    let author = $(`#author`).text();
+    // author 와 현재 유저네임이 같은지 확인합니다.
+    let cur_username = $('#username').text();
+    if (cur_username != author) {
+        alert("자신이 작성한 글만 수정이 가능합니다")
+        return;
+    }
     // 작성한 포스트가 올바른지 isValidContents 함수를 통해 확인합니다.
     if (isValidContents(contents) == false) {
         return;
     }
-    if (isValidTitle(title) == false) {
-        return;
-    }
     // 전달할 data JSON으로 만듭니다.
     let data = {'title': title, 'author': author, 'contents': contents};
-    // PUT /api/memos/{id} 에 data를 전달합니다.
     $.ajax({
         type: "PUT",
         url: `/api/posts/${id}`,
@@ -129,24 +129,23 @@ function submitEdit() {
     });
 }
 
-// 포스트를 삭제합니다.
+// 메모를 삭제합니다.
 function deleteOne() {
-    $('#delete').on('click', function () {
-        let author = $('.author').text()
-        let cur_username = $('.username01').text()
+    let id = location.search.split('=')[1]
+    let author = $('#author').text()
+    let cur_username = $('#username').text()
 
-        if (cur_username != author) {
-            alert("자신이 작성한 글만 삭제가 가능합니다")
-            return;
+    if (cur_username != author) {
+        alert("자신이 작성한 글만 삭제가 가능합니다")
+        return;
+    }
+
+    $.ajax({
+        type: "DELETE",
+        url: `/api/posts/${id}`,
+        success: function (response) {
+            alert('게시글 삭제에 성공하였습니다.');
+            window.location.href = "/";
         }
-        let id = location.search.split('=')[1]
-        $.ajax({
-            type: "DELETE",
-            url: `/api/posts/${id}`,
-            success: function (response) {
-                alert('게시글 삭제에 성공하였습니다');
-                window.location.href = "/ ";
-            }
-        })
     })
 }
